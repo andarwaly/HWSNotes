84039b9a4105e3ff535c85175cb0544c0b9f0053
Dzaky Waly <andarwaly@gmail.com>
2024-10-16 18:55:37 +0700

    Initial Commit

---
+++ HWSNotes/HWSNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSNotesApp.swift
+//  HWSNotes
+//
+//  Created by Muhammad Dzaky on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HWSNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
